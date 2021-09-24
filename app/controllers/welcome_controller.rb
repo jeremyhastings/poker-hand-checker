0a397cbd7c59d7c53a3096ed1fb455945c7f5502
Jeremy Hastings <jeremy.hastings@mac.com>
2021-09-24 13:46:41 -0500

    Form Working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,8 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    respond_to do |format|
+      format.html
+      format.js { render(partial: 'index') }
+    end
+  end
 end
