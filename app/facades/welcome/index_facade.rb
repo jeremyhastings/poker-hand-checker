a507f0ca1e95a5afbfe699b26e17777915796b3e
Jeremy Hastings <jeremy.hastings@mac.com>
2021-09-26 09:14:39 -0500

    Validates Unique Entries, Card Count, and Separates Values for Cards.

--- app/facades/welcome/index_facade.rb
+++ app/facades/welcome/index_facade.rb
@@ -1,7 +1,39 @@
 module Welcome
   class IndexFacade < ApplicationFacade
-    def cards
-      @cards ||= params[:poker_hand][:cards]
+    def process_hand
+      return 'Invalid Hand' unless validates_uniqueness_and_count
+
+      separate_face_and_suit
+      processed_data
+    end
+
+    private
+
+    def validates_uniqueness_and_count
+      raw_data.count == 5 && raw_data.uniq.count == 5
+    end
+
+    def separate_face_and_suit
+      raw_data.uniq.each do |data|
+        processed_data << (data.length == 2 ? [data.first, data.last] : [data.first(2), data.last])
+      end
+    end
+
+    def raw_data
+      @raw_data ||= (params[:poker_hand][:cards]).upcase.split
+    end
+
+    def processed_data
+      @processed_data ||= []
+    end
+
+    # def validates_face
+    #   str = '10C'
+    #   str.length == 2 ? str.first : str.first(2)
+    # end
+    #
+    # def validates_suit
+    #   'AH'.last
     end
   end
 end
